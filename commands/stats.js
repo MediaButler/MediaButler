bd085be9b6aeb3e8bcefdb40e90395895d7d4117
Chris <chris@Chris-Retina.local>
2017-11-13 01:44:56 +0000

    little bit of a clean up

--- commands/stats.js
+++ commands/stats.js
@@ -8,28 +8,28 @@ exports.run = (bot, msg, args, perms = []) => {
     msg.channel.send('No user selected.');
   } else {
     msg.channel.startTyping();
-var getUsersUrl = 'http://' + apiauth.plexpy_host + apiauth.plexpy_baseurl + '/api/v2?apikey=' + apiauth.plexpy_apikey + '&cmd=get_users';
-
-request(getUsersUrl, function (e, r, b) {
-var j = JSON.parse(b)
-var userid = j.response.data.find(o => o.username == args[0]).user_id;
-msg.channel.send("UserID: " + userid);
-if (userid == undefined)
-{
-  msg.channel.send("Unable to match user");
-}
-else {
-    var url = 'http://' + apiauth.plexpy_host + apiauth.plexpy_baseurl + '/api/v2?apikey=' + apiauth.plexpy_apikey + '&cmd=get_user_watch_time_stats&user_id=' + userid;
-    var embed_fields = [];
-    request(url, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var info = JSON.parse(body)
-      	info.response.data.forEach(i =>
-        {
-          embed_fields.push({ "name": "Last " + i.query_days + " Days", "value": durationFormat(i.total_time) + " / " + i.total_plays + " items", "inline": true })
-        });
-        msg.channel.send({
-            "embed": {
+    
+    var getUsersUrl = 'http://' + apiauth.plexpy_host + apiauth.plexpy_baseurl + '/api/v2?apikey=' + apiauth.plexpy_apikey + '&cmd=get_users';
+    request(getUsersUrl, function (e, r, b) {
+    var j = JSON.parse(b)
+    var userid = j.response.data.find(o => o.username == args[0]).user_id;
+    msg.channel.send("UserID: " + userid);
+    if (userid == undefined)
+    {
+      msg.channel.send("Unable to match user");
+    }
+    else {
+      var url = 'http://' + apiauth.plexpy_host + apiauth.plexpy_baseurl + '/api/v2?apikey=' + apiauth.plexpy_apikey + '&cmd=get_user_watch_time_stats&user_id=' + userid;
+      var embed_fields = [];
+      request(url, function (error, response, body) {
+       if (!error && response.statusCode == 200) {
+         var info = JSON.parse(body)
+         info.response.data.forEach(i =>
+         {
+           embed_fields.push({ "name": "Last " + i.query_days + " Days", "value": durationFormat(i.total_time) + " / " + i.total_plays + " items", "inline": true })
+         });
+         msg.channel.send({
+           "embed": {
             "color": 7221572,
             "timestamp": new Date(),
             "footer": {
@@ -41,12 +41,11 @@ else {
             },
             "fields": embed_fields
             }
-        });
-      } else { msg.channel.send('error: ' + error); } // if(!error
-    msg.channel.stopTyping();
-    });
-  }
-
+          });
+        } else { msg.channel.send('error: ' + error); } // if(!error
+      msg.channel.stopTyping();
+      });
+    }
   });
   }
 };
