78fd6695d15ea079370ef1498d79d57ec72d44b5
Chris <chris@Chris-Retina.local>
2017-11-13 01:32:19 +0000

    should also comment out notes

--- commands/stats.js
+++ commands/stats.js
@@ -10,7 +10,7 @@ exports.run = (bot, msg, args, perms = []) => {
     msg.channel.startTyping();
 // get users
 
-arr.find(o => o.city === 'Amsterdam'); 
+//arr.find(o => o.city === 'Amsterdam'); 
 var getUsersUrl = 'http://' + apiauth.plexpy_host + apiauth.plexpy_baseurl + '/api/v2?apikey=' + apiauth.plexpy_apikey + '&cmd=get_users';
 
 request(getUsersUrl, function (e, r, b) {
