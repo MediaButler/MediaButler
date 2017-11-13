472a5f99ce0e23c440c8e318362055ba2c378bc8
Chris <chris@Chris-Retina.local>
2017-11-13 19:08:23 +0000

    console log error for testing purposes, it kept showing Error, but it might say something else once it a while.

--- commands/addtv.js
+++ commands/addtv.js
@@ -56,7 +56,7 @@ exports.run = (client, message, args, perms) => {
       sonarr.post("series", { "data": data }).then(function (postResult){
         console.log(postResult);
         message.channel.send("I think we added it");
-      }).catch(error => { message.channel.send("Sorry, an unknown error occured, please check Sonarr logs")});
+      }).catch(error => { console.log(error); message.channel.send("Sorry, an unknown error occured, please check Sonarr logs")});
   });
 };
 
