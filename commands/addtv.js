ad9c07dff57572a07bd89c43f7ba171a74ba96bb
Chris <chris@Chris-Retina.local>
2017-11-13 16:32:49 +0000

    get started with addtv

--- commands/addtv.js
+++ commands/addtv.js
@@ -1,6 +1,6 @@
 const apiauth = require('../apiauth.json');
-const SonarrAPI = require('../node_modules/sonarr-api/lib/api.js');
-const sonarr = new SonarrAPI({
+var SonarrAPI = require('../node_modules/sonarr-api/lib/api.js');
+var sonarr = new SonarrAPI({
   hostname: apiauth.sonarr_host.split(":")[0],
   apiKey: apiauth.sonarr_apikey,
   port: apiauth.sonarr_host.split(":")[1],
@@ -8,14 +8,33 @@ const sonarr = new SonarrAPI({
 });
 
 exports.run = (client, message, args, perms) => {
-  let tvdbId;
-  let showTitle;
-  let qualityProfile;
-  let rootFolderPath;
+  var tvdbId;
+  var showTitle;
+  var qualityProfile;
+  var rootFolderPath;
+  var profileId = apiauth.sonarr_defaultProfileid;
+  var rootPath = apiauth.sonarr_defaultRootPath;
 
-// Lookup id
-// getQualityProfileId
-// getRootFolderPath;
+  if (!args[0]) {
+    message.channel.send("No variables found. run `.help addtv`");
+  }
+
+  if (args[1]) {
+    sonarr.get("profile").then(function (result) {
+      profileId - result.find(q => q.name == args[1]).id;
+      if (profileId == undefined) {
+        message.client.channel("Profile not found.");
+      }
+    });
+  }
+
+  if (args[2]) {
+    rootPath = args[2];
+  }
+
+  sonarr.get("series/lookup", { "term": "tvdb:" + args[0]}).then(function (result) {
+      // Add show to sonarr
+  });
 };
 
 exports.conf = {
