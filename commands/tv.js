80e39d04127bd09e85575b5a351c144c90b12294
Chris <chris@Chris-Retina.local>
2017-11-14 23:48:08 +0000

    migrate tv to use internal sonarr rather then requiring tvdb api key

--- commands/tv.js
+++ commands/tv.js
@@ -1,82 +1,87 @@
 const apiauth = require('../apiauth.json');
-const TVDB = require('node-tvdb');
-const tvdb = new TVDB(apiauth.thetvdbkey);
+const SonarrAPI = require('../node_modules/sonarr-api/lib/api.js');
+const sonarr = new SonarrAPI({
+  hostname: apiauth.sonarr_host.split(":")[0],
+  apiKey: apiauth.sonarr_apikey,
+  port: apiauth.sonarr_host.split(":")[1],
+  urlBase: apiauth.sonarr_baseurl
+});
 
 exports.run = (bot, msg, args = []) => {
   const max = 4462;
 
-  tvdb.getSeriesByName(args.join(" "))
-      .then(titleSearchResponse => {
-        tvdb.getSeriesById(titleSearchResponse[0].id)
-            .then(response => {
-              msg.channel.send({
-                    "content": "As requested....",
-                    "embed": {
-                      "title": response.seriesName,
-                      "description": response.overview,
-                      "color": 13619085,
-                      "timestamp": new Date(),
-                      "footer": {
-                        "icon_url": msg.author.avatarURL,
-                        "text": "Called by " + msg.author.username
-                      },
-                      "image": {
-                        "url": "http://thetvdb.com/banners/" + response.banner
-                      },
-                      "author": {
-                        "name": response.seriesName,
-                        "url": "https://www.thetvdb.com/?tab=series&id=" + response.id,
-                        "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
-                      },
-                      "fields": [
-                        {
-                          "name": "Network",
-                          "value": response.network,
-                          "inline": true
-                        },
-                        {
-                          "name": "First Aired",
-                          "value": response.firstAired,
-                          "inline": true
-                        },
-                        {
-                          "name": "Airs on",
-                          "value": response.airsDayOfWeek + " " + response.airsTime,
-                          "inline": true
-                        },
-                        {
-                          "name": "Genres",
-                          "value": response.genre.join(', '),
-                          "inline": true
-                        },
-                        {
-                          "name": "Status",
-                          "value": response.status,
-                          "inline": true
-                        },
-                        {
-                          "name": "Rating",
-                          "value": response.siteRating + " (" + response.siteRatingCount + " votes)",
-                          "inline": true
-                        },
-                        {
-                          "name": "Runtime",
-                          "value": response.runtime + " mins",
-                          "inline": true
-                        },
-                        {
-                          "name": "TVDb ID",
-                          "value": response.id,
-                          "inline": true
-                        }
-                      ]
-                    }
-                  }
-              )
-            })
-      }).catch(error => {
-    msg.channel.send("No results in the TVDB database. Edit the show name or add it yourself over at thetvdb.com!")
-  })
+  sonarr.get("series/lookup", { "term": args.join(" ") }).then(function (result) {
+    if (result.length === 0) {
+      message.chanel.send("Unable to pull show matching that ID");
+    }
+
+    msg.channel.send(
+      {
+        "embed": 
+        {
+          "title": result[0].title,
+          "description": result[0].overview,
+          "color": 13619085,
+          "timestamp": new Date(),
+          "footer": {
+            "icon_url": msg.author.avatarURL,
+            "text": "Called by " + msg.author.username
+          },
+          "image": {
+            "url": "http://thetvdb.com/banners/" + result[0].images.banner.url
+          },
+          "author": {
+            "name": "TV Show Information",
+            "url": "https://www.thetvdb.com/?tab=series&id=" + result[0].tvdbId,
+            "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+          },
+          "fields": 
+          [
+            {
+              "name": "Network",
+              "value": result[0].network,
+              "inline": true
+            },
+            {
+              "name": "First Aired",
+              "value": result[0].firstAired,
+              "inline": true
+            },
+            {
+              "name": "Airs on",
+              "value": result[0].airsDayOfWeek + " " + result[0].airsTime,
+              "inline": true
+            },
+            {
+              "name": "Genres",
+              "value": result[0].genre.join(', '),
+              "inline": true
+            },
+            {
+              "name": "Status",
+              "value": result[0].status,
+              "inline": true
+            },
+            {
+              "name": "Rating",
+              "value": result[0].siteRating + " (" + result[0].siteRatingCount + " votes)",
+              "inline": true
+            },
+            {
+              "name": "Runtime",
+              "value": result[0].runtime + " mins",
+              "inline": true
+            },
+            {
+              "name": "TVDb ID",
+              "value": result[0].tvdbId,
+              "inline": true
+            }
+          ]
+        }
+      }
+    )
+  });
 };
 
 exports.conf = {
