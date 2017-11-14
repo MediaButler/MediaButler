67d85815904fd3b3024081f3902d19f8b9ed17f8
Chris van der geld <Chris@toppershops.nl>
2017-11-14 09:23:29 +0100

    Changed data to info.data for jsonParse

--- commands/anime.js
+++ commands/anime.js
@@ -9,20 +9,20 @@ exports.run = (bot, msg, args = []) => {
     const url = 'https://kitsu.io/api/edge/anime?filter%5Btext%5D=' + query;
     request(url, function (error, res, body) {
       if (!error && res.statusCode === 200) {
-        let data = JSON.parse(body);
-        if (data[0] === undefined) {
+        let info = JSON.parse(body);
+        if (info.data[0] === undefined) {
           msg.channel.send('Cant find anime.')
         } else {
 
-           const overview = data[0].attributes.synopsis === undefined ? "No description" : data[0].attributes.synopsis;
+           const overview = info.data[0].attributes.synopsis === undefined ? "No description" : info.data[0].attributes.synopsis;
 
           let trimmedOverview = overview.substring(0, 550);
 
           msg.channel.send({
                 "content": "As requested....",
                 "embed": {
-                  "title": data[0].attributes.titles.en,
-                  "description": trimmedOverview + "... https://kitsu.io/anime/" + data[0].attributes.slug,
+                  "title": info.data[0].attributes.titles.en,
+                  "description": trimmedOverview + "... https://kitsu.io/anime/" + info.data[0].attributes.slug,
                   "color": 11360941,
                   "timestamp": new Date(),
                   "footer": {
@@ -30,32 +30,32 @@ exports.run = (bot, msg, args = []) => {
                     "text": "Called by " + msg.author.username
                   },
                   "image": {
-                    "url": data[0].attributes.posterImage.tiny + ".jpg",
+                    "url": info.data[0].attributes.posterImage.tiny + ".jpg",
                   },
                   "author": {
-                    "name": data[0].attributes.titles.en,
-                    "url": "https://kitsu.io/anime/" + data[0].attributes.slug,
+                    "name": info.data[0].attributes.titles.en,
+                    "url": "https://kitsu.io/anime/" + info.data[0].attributes.slug,
                     "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
                   },
                   "fields": [
                     {
                       "name": "Anime",
-                      "value": data[0].attributes.titles.en,
+                      "value": info.data[0].attributes.titles.en,
                       "inline": true
                     },
                     {
                       "name": "Age Rating Guide",
-                      "value": data[0].attributes.ageRatingGuide,
+                      "value": info.data[0].attributes.ageRatingGuide,
                       "inline": true
                     },
                     {
                       "name": "Status",
-                      "value": data[0].attributes.status,
+                      "value": info.data[0].attributes.status,
                       "inline": true
                     },
                     {
                       "name": "Kitsu ID",
-                      "value": data[0].id,
+                      "value": info.data[0].id,
                       "inline": true
                     }
                   ]
@@ -76,6 +76,6 @@ exports.conf = {
 };
 exports.help = {
   name: "anime",
-  description: "Pulls data for an anime",
+  description: "Pulls info for an anime",
   usage: "anime <anime name>"
 };
