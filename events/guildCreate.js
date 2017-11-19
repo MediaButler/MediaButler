bb1ec8ffc5bb73d4d6c23be3a44d5dad889dab03
Chris <chris@Chris-Retina.local>
2017-11-19 20:56:15 +0000

    console log guildId

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,5 +1,12 @@
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./settings.sqlite', (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+  });
+
 module.exports = (guild, client) => {
     // We have joined a new discord server, we need to populate and prepare for configuration.
     // Looking at a db backend, possibly sqlite to hold the "server configurations"
-    console.log(guild);
+    console.log(guild.id);
 };
