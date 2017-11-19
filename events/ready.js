4ec99ccca97a8daccd5cf0efab1b2fdeb0e2e884
Chris <chris@Chris-Retina.local>
2017-11-19 19:08:24 +0000

    move setGame to ready.js, should not be set in app.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 const chalk = require('chalk');
 module.exports = client => { // eslint-disable-line no-unused-vars
   console.log(chalk.bgGreen.black('I\'m Online'));
+  client.user.setPresence({game: {name: "MediaButler v0.1", type: 0}});  
 };
