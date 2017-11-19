eb03f19b311cd3e60f553cbbb8390157b38b4056
Chris <chris@Chris-Retina.local>
2017-11-19 22:24:38 +0000

    create initial settings on joining of a new guild, allows set to work maybe?

--- commands/settings.js
+++ commands/settings.js
@@ -1,6 +1,19 @@
 const sqlite3 = require('sqlite3').verbose();
-exports.run = (client, message, params, perms) => {
-      message.channel.send("Hello, World!");
+let db = new sqlite3.Database('./settings.sqlite');
+
+exports.run = (client, message, params = [], perms) => {
+    let setting = params[0];
+    let value = params[1];
+    let guildId = message.guild.id;
+    let query = `UPDATE guildSettings SET "value" = ? WHERE "guildId" = ? AND "setting" = ?`
+    let queryData = [value, guildId, setting];
+    db.run(query, queryData, function(err) {
+        if (err) {
+            message.channel.send("Unable to update: " + err.message);
+          return;
+        }
+        message.channel.send("Sucessfully Updated");
+    });
 };
 
 exports.conf = {
--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,12 +1,32 @@
 const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('./settings.sqlite', (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-  });
+let db = new sqlite3.Database('./settings.sqlite');
 
 module.exports = (guild, client) => {
     // We have joined a new discord server, we need to populate and prepare for configuration.
     // Looking at a db backend, possibly sqlite to hold the "server configurations"
-    console.log(guild.id);
+    const guildId = guild.id;
+    db.serialize(() => 
+    {
+        db.run(`INSERT INTO greetings("guildId", "setting")
+            VALUES(guildId, "plexpy.host"),
+                (guildId, "plexpy.baseurl"),
+                (guildId, "plexpy.apikey"),
+                (guildId, "sonarr.host"),
+                (guildId, "sonarr.baseurl"),
+                (guildId, "sonarr.apikey"),
+                (guildId, "radarr.host"),
+                (guildId, "radarr.baseurl"),
+                (guildId, "radarr.apikey"),
+                (guildId, "lidarr.host"),
+                (guildId, "lidarr.baseurl"),
+                (guildId, "lidarr.apikey"),
+                (guildId, "tmdb.apikey"),
+                (guildId, "tvdb.apikey"),
+                (guildId, "self.adminChannel"),
+                (guildId, "self.adminRole"),
+                (guildId, "self.modRole")
+        `);
+    });
+    db.close();
+    guild.owner.send("Hello, I am ${client.user.username} your personal Media Butler!\nTo get going we are going to need to set a few settings, please look at the set command and our github wiki to get going");
 };
