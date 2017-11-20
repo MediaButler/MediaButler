63a4e912528f51482d39ceb836f3e2ed9dc37a4a
Chris <chris@Chris-Retina.local>
2017-11-20 00:43:50 +0000

    getSettings tests

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const chalk = require('chalk');
 const fs = require('fs');
 const moment = require('moment');
 require('./util/eventLoader')(client);
+require('./services/getSettings.js');
 
 const log = message => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
