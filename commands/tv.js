77e09d158546f736ca6efb8e599d40db81fc427c
Chris <chris@Chris-Retina.local>
2017-11-20 21:37:10 +0000

    clean this out too temporarily

--- commands/tv.js
+++ commands/tv.js
@@ -26,7 +26,7 @@ exports.run = (bot, msg, args = []) => {
         },
         "author": {
           "name": "TV Show Information",
-          "url": `https://www.thetvdb.com/?tab=series&id=${tvShow.tvdbId}`,
+          "url": `https://www.thetvdb.com/?tab=series&id=`,
           "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
         },
         "fields": [
@@ -64,12 +64,12 @@ exports.run = (bot, msg, args = []) => {
             "name": "IMDb ID",
             "value": info.imdbID,
             "inline": true
-          },
-          {
-            "name": "TVDb ID",
-            "value": tvShow.tvdbId,
-            "inline": true
           }
+//          {
+//            "name": "TVDb ID",
+//            "value": tvShow.tvdbId,
+//            "inline": true
+//          }
         ]}
       })
     }
