a2d177cf35fab70ac09e6c3c018e35e818ef2592
Chris <chris@Chris-Retina.local>
2017-11-20 02:09:23 +0000

    fix up guildDelete, can reuse without restarting

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,10 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('./settings.sqlite');
 
 module.exports = (guild, client) => {
     // We have joined a new discord server, we need to populate and prepare for configuration.
     // Looking at a db backend, possibly sqlite to hold the "server configurations"
     const guildId = guild.id;
+    let db = new sqlite3.Database('./settings.sqlite');    
     db.serialize(() => 
     {
         db.run(`DELETE FROM guildSettings WHERE guildId = ${guildId}`);
