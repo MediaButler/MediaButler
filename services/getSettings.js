14c6d18a121522499ed7dd200e81171edb896bd5
Chris <chris@Chris-Retina.local>
2017-11-20 01:17:43 +0000

    module.run ?

--- services/getSettings.js
+++ services/getSettings.js
@@ -1,6 +1,6 @@
 var sqlite3 = require('sqlite3').verbose();
 
-module.exports = function (guildId)
+module.run = function (guildId)
 {
     let db = new sqlite3.Database('./settings.sqlite');    
     let sql = `SELECT setting,value FROM guildSettings WHERE guildId = ${guildId}`;
