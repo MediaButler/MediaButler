fb5eeec55d7b956d5a229a97b9e19e3982d734e6
Chris <chris@Chris-Retina.local>
2017-11-20 01:52:17 +0000

    promises on getSettings... Still not loading in app

--- services/getSettings.js
+++ services/getSettings.js
@@ -3,9 +3,20 @@ let db = new sqlite3.Database('./settings.sqlite');
 
 module.exports = (guildId) =>
 {
-    let sql = `SELECT setting,value FROM guildSettings WHERE guildId = ${guildId}`;
-    db.all(sql, function(err, rows) {
-        return JSON.stringify(rows);
-        db.close();
-    });
+    const p = new Promise(
+        function (resolve, reject) {
+            let sql = `SELECT setting,value FROM guildSettings WHERE guildId = ${guildId}`;
+            db.all(sql, function(err, rows) {
+                if (err)
+                {
+                    reject(err);
+                }
+                else
+                {
+                    resolve(JSON.stringify(rows));
+                }
+                db.close();
+            });
+        });
+    return p;
 }
