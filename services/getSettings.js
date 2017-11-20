2c87445a84bf4a7250d5bb1ec2af89f757def077
Chris <chris@Chris-Retina.local>
2017-11-20 01:06:33 +0000

    hmm

--- services/getSettings.js
+++ services/getSettings.js
@@ -4,8 +4,9 @@ module.exports = function (guildId)
 {
     let db = new sqlite3.Database('./settings.sqlite');    
     let sql = `SELECT setting,value FROM guildSettings WHERE guildId = ${guildId}`;
+    let output;
     db.all(sql, function(err, rows) {
-        let output = JSON.stringify(rows);
+        output = JSON.stringify(rows);
     });
     db.close();
     return output;
