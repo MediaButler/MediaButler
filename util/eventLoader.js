aceb78e8c51a595afcbdac103946a4993824bd0a
Chris <chris@Chris-Retina.local>
2017-11-21 22:56:06 +0000

    little more cleanup

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -4,10 +4,6 @@ module.exports = client => {
   client.on('reconnecting', () => reqEvent('reconnecting')(client));
   client.on('disconnect', () => reqEvent('disconnect')(client));
   client.on('message', reqEvent('message'));
-//  client.on('guildMemberAdd', reqEvent('guildMemberAdd'));
-//  client.on('guildMemberRemove', reqEvent('guildMemberRemove'));
-//  client.on('guildBanAdd', reqEvent('guildBanAdd'));
-//  client.on('guildBanRemove', reqEvent('guildBanRemove'));
   client.on('guildCreate', reqEvent('guildCreate'));
   client.on('guildDelete', reqEvent('guildDelete'));  
 };
