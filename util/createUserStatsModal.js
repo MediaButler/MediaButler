57ae39345909beb6931dbd20f6e5216325c88df7
Chris <chris@Chris-Retina.local>
2017-11-22 00:56:10 +0000

    maybe?

--- util/createUserStatsModal.js
+++ util/createUserStatsModal.js
@@ -8,7 +8,7 @@ module.exports = (userStats) =>
     .setColor(7221572)
     .setTimestamp()
     .setAuthor("Statistics");
-    userStats.forEach(i => {
+    userStats.data.forEach(i => {
         e.addField(`Last ${i.query_days} Days`, `${durationFormat(i.total_time)} / ${i.total_plays} items`, true);
     });
     return e;
