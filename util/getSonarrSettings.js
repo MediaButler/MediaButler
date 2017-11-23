f7bcfb556143ca4b97b91d427b865ce98c4c4835
Chris <chris@Chris-Retina.local>
2017-11-23 03:00:26 +0000

    move sonarr to url pattern, update tonight to test

---
+++ util/getSonarrSettings.js
@@ -0,0 +1,27 @@
+const getSettings = require('./getSettings');
+module.exports = (guildId) =>
+{
+    const p = new Promise((resolve, reject) => 
+    {
+        getSettings(guildId)
+        .then((settings) =>
+            {
+                settings = JSON.parse(settings);
+                let url = settings.find(x => x.setting == "sonarr.url");
+                let apikey = settings.find(x => x.setting == "sonarr.apikey");
+                const regex = /^(http[s]?):\/?\/?([^:\/\s]+):?([0-9]{4})?((\/\w+)*\/)([\w\-\.]+[^#?\s]+)?$/g;
+                let details = regex.exec(url.value);
+                if (url.value == null || apikey.value == null) reject("Sonarr not configured");
+                let i = {};
+                i.protocol = details[1];
+                i.host = details[2];
+                i.port = 443;
+                if (details[3] !== undefined) i.port = details[3];
+                i.path = details[6];
+                i.apikey = apikey.value;
+                resolve(i);
+            }
+        );
+    });
+    return p;
+}
