faa5d797ca430fd8ae86eb77fd673580e1430119
Chris <chris@Chris-Retina.local>
2017-11-23 02:01:32 +0000

    ok, so now we have some kind of output, lets see if we get an output we want

--- util/getPlexPySettings.js
+++ util/getPlexPySettings.js
@@ -13,8 +13,13 @@ module.exports = (guildId) =>
                 const regex = /^(http[s]?):\/?\/?([^:\/\s]+):?([0-9]{4})?((\/\w+)*\/)([\w\-\.]+[^#?\s]+)?$/g;
                 let details = regex.exec(url.value);
                 if (url === undefined || apikey === undefined) reject("plexpy settings not set");
-                console.log(details);
-                resolve(details);
+                let i = {};
+                i.protocol = details[1];
+                i.host = details[2];
+                if (details[3] === undefined) i.host = i.host + `:${details[3]}`;
+                i.url = details[6];
+                console.log(i);
+                resolve(i);
             }
         );
     });
