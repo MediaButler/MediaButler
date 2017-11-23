93c1bf0043b1ee1ce8949831f68ce9cc108350e0
Chris <chris@Chris-Retina.local>
2017-11-23 04:10:15 +0000

    Well.. this should complete the addfilm function.

--- util/createMovieItemFromRadarr.js
+++ util/createMovieItemFromRadarr.js
@@ -1,7 +1,15 @@
 module.exports = (movie) =>
 {
-    let d = new Date(tvShow.firstAired);
+    let d = new Date(movie.inCinemas);
     let i = {};
-
+    i.Year = movie.year;
+    i.Poster = movie.remotePoster;
+    i.Rated = "N/A";
+    i.Released = d.toDateString();
+    i.Genre = movie.genres.join(", ");
+    i.Runtime = movie.runtime;
+    i.imdbRating = movie.ratings.value;
+    i.imdbVotes = movie.ratings.votes;
+    i.imdbID = `N/A (TMDb ${movie.tmdbId})`;
     return i;
 }
