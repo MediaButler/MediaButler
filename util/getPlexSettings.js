aa2560cdce53f10301af9a51b79472b9a26f5dfa
Chris <chris@Chris-Retina.local>
2017-11-27 23:23:52 +0000

    add UUID requirement into plexSettings.

--- util/getPlexSettings.js
+++ util/getPlexSettings.js
@@ -10,13 +10,16 @@ module.exports = (guildId) =>
             let url = settings.find(x => x.setting == "plex.url");
             let token = settings.find(x => x.setting == "plex.token");
             let pinToken = settings.find(x => x.setting == "plex.pintoken");
+            let uuid = settings.find(x => x.setting == "plex.uuidv4");
             const regex = /^(http[s]?):\/?\/?([^:\/\s]+):?([0-9]{4})?((\/\w+)*\/)([\w\-\.]+[^#?\s]+)?$/g;
+            if (uuid == undefined) reject("UUID not configured, please delete and re-invite this bot");
             let details = regex.exec(url.value);
             if (url.value == null) reject("Plex not configured");
             let i = {};
             i.host = url.value;
             i.token = token.value;
             i.pinToken = null;
+            i.uuid = uuid.value;
             if (pinToken.value != null) i.pinToken = JSON.parse(pinToken.value.replace(/\\"/g, '"'));
             resolve(i);
         });
