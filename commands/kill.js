0b77c2bdf7acbcd523094ac3d6093fe7197718f2
Chris <chris@Chris-Retina.local>
2017-11-28 21:20:58 +0000

    get rid of the console dump now it works :D

--- commands/kill.js
+++ commands/kill.js
@@ -5,11 +5,8 @@ exports.run = (bot, msg, args, perms) => {
     .then((m) => {
         getPlexClient(msg.guild.id)
         .then((plexClient) => {
-            console.log(plexClient);
             m.edit("We have a fully authenticated plex token. Thats the end of the command");
         }).catch((err) => {
-            console.log(err);
-            console.log(typeof(err));
             if (err == "updTokenSuccessful") m.edit("Sucessfully processed plex token. Please run command again and we will work.");
             if (typeof(err) == "object") m.edit(`Please go to https://plex.tv/pin and authenticate this code: ${err.code}`);
         })
