1d4ccb58d46234010c68182c86ac83e91c29ea16
Chris <chris@Chris-Retina.local>
2017-11-28 21:05:06 +0000

    move catch to after then... again

--- util/plex/getPlexClient.js
+++ util/plex/getPlexClient.js
@@ -10,8 +10,8 @@ module.exports = (guildId) =>
         getSettings(guildId)
         .then((settings) => {
             if (settings.token == null || settings.token == "") {
-                if (settings.pinToken == null || settings.pinToken == "") getPin(guildId).then((pinobj) => { console.log(pinobj); throw JSON.stringify(pinobj); return; });
-                else getAuthToken(guildId).then(() => { throw "updTokenSuccessful"; return; });
+                if (settings.pinToken == null || settings.pinToken == "") getPin(guildId).then((pinobj) => { console.log(pinobj); reject(pinobj); return; });
+                else getAuthToken(guildId).then(() => { reject("updTokenSuccessful"); return; });
             }
             let opts = {};
             opts.options = {};
