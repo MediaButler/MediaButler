b38803488263cfb8f73462908ca56b87a14c2a60
Chris <chris@Chris-Retina.local>
2017-11-28 02:41:13 +0000

    Preparation for guildCreate script. This should make event/guildCreate.js a lot smaller

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,15 @@
 const chalk = require('chalk');
+const getSettings = require('../util/getSettings');
 module.exports = client => { // eslint-disable-line no-unused-vars
   console.log(chalk.bgGreen.black('I\'m Online'));
-  client.user.setPresence({game: {name: "MediaButler v0.2", type: 0}});  
+  client.user.setPresence({game: {name: "MediaButler v0.2", type: 0}});
+  
+  client.guilds.forEach((g) => {
+    getSettings(g.id)
+    .then((res) => {
+      console.log(`Loaded configuration for ${g.id}`);
+    }).catch((err) => {
+      // RUN GUILD CREATE SCRIPT
+    });
+  });
 };
