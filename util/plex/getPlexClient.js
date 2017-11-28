0c584ff5def002cd0eb53820bb40c61aca1a7be4
Chris <chris@Chris-Retina.local>
2017-11-28 21:16:57 +0000

    hide resolve, just incase it overrules the reject()

--- util/plex/getPlexClient.js
+++ util/plex/getPlexClient.js
@@ -13,18 +13,20 @@ module.exports = (guildId) =>
                 if (settings.pinToken == null || settings.pinToken == "") getPin(guildId).then((pinobj) => { reject(pinobj); return; });
                 else getAuthToken(guildId).then(() => { reject("updTokenSuccessful"); return; });
             }
-            let opts = {};
-            opts.options = {};
-            opts.hostname = settings.host;
-            opts.https = true;
-            opts.authToken = settings.token;
-            opts.options.identifier = settings.uuid;
-            opts.options.product = 'MediaButler';
-            opts.options.version = '0.2';
-            opts.options.deviceName = 'MediaButlerBot';
-            opts.authenticator = plexPinAuth;
-            d = new plexApi(opts);
-            resolve(d);
+            else {
+                let opts = {};
+                opts.options = {};
+                opts.hostname = settings.host;
+                opts.https = true;
+                opts.authToken = settings.token;
+                opts.options.identifier = settings.uuid;
+                opts.options.product = 'MediaButler';
+                opts.options.version = '0.2';
+                opts.options.deviceName = 'MediaButlerBot';
+                opts.authenticator = plexPinAuth;
+                d = new plexApi(opts);
+                resolve(d);
+            }
         }).catch((err) => { console.log(err); });
     });
     return p;
