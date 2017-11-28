1c5e079de799a0a955bad5a3f68d4081c6271d0b
Chris <chris@Chris-Retina.local>
2017-11-28 21:36:51 +0000

    actually do something with plex api now we created it correctly (i think)

--- commands/kill.js
+++ commands/kill.js
@@ -1,11 +1,17 @@
 const getPlexClient = require('../util/plex/getPlexClient');
+const plexApi = require('plex-api');
 
 exports.run = (bot, msg, args, perms) => {
     msg.channel.send("Starting...")
     .then((m) => {
         getPlexClient(msg.guild.id)
         .then((plexClient) => {
+            plexClient.query("/status/sessions")
+            .then((res) => {
+                console.log(res);
+            });
             m.edit("We have a fully authenticated plex token. Thats the end of the command");
+            
         }).catch((err) => {
             if (err == "updTokenSuccessful") m.edit("Sucessfully processed plex token. Please run command again and we will work.");
             if (typeof(err) == "object") m.edit(`Please go to https://plex.tv/pin and authenticate this code: ${err.code}`);
