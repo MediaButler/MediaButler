79bdf32d1b07ea382d3201b2d1644977b119fcda
Chris <chris@Chris-Retina.local>
2017-11-28 00:47:41 +0000

    Stub code for kill command

---
+++ commands/kill.js
@@ -0,0 +1,17 @@
+exports.run = (client, message, args, perms) => {
+    message.channel.send("Hello, World!");
+  };
+  
+  exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 2
+  };
+  
+  exports.help = {
+    name: 'kill',
+    description: 'Kills stream playing from Plex. Gives optional reason',
+    usage: 'kill <streamId> [reason]'
+  };
+  
