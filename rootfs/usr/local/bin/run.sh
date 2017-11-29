a55836dc9cd4e4a7a27c55918c03eb3cefbd213a
Starbix <cedric@laubacher.io>
2017-11-29 17:52:42 +0100

    Use alpine as baseimage

---
+++ rootfs/usr/local/bin/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Updating permissions..."
+for dir in /opt/MediaButler /etc/s6.d; do
+  if $(find $dir ! -user $UID -o ! -group $GID|egrep '.' -q); then
+    echo "Updating permissions in $dir..."
+    chown -R $UID:$GID $dir
+  else
+    echo "Permissions in $dir are correct."
+  fi
+done
+echo "Done updating permissions."
+
+sed -i 's/TOKEN_HERE/'"$TOKEN"'/g' /opt/MediaButler/settings.json
+sed -i 's/PREFIX_HERE/'"$PREFIX"'/g' /opt/MediaButler/settings.json
+
+su-exec $UID:$GID /bin/s6-svscan /etc/s6.d
