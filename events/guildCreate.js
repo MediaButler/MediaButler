8762ce893d76750d7456f6bdb094097b1c6f5ca8
Vertig0ne <chris@Chris-Retina.local>
2017-12-03 23:50:49 +0000

    some cleaning.

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,9 +1,7 @@
-const sqlite3 = require('sqlite3').verbose();
-const uuid = require('uuid-v4');
 const guildCreate = require('../util/discordCreateGuild');
 module.exports = (guild) => {
     guildCreate(guild.id)
     .then(() => {
         guild.owner.send(`Hello, I am ${guild.client.user.username} your personal Media Butler!\nTo get going we are going to need to set a few settings, please look at the set command and our github wiki to get going`);        
-    }).catch((err) => { console.log(`ERR: Unable to setup guild for ${guild.id}`); });
+    }).catch(() => { console.log(`ERR: Unable to setup guild for ${guild.id}`); });
 };
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const coreSettings = require(`${process.cwd()}/settings.json`);
 
-module.exports = (guild, client) => {
+module.exports = (guild) => {
     // We have joined a new discord server, we need to populate and prepare for configuration.
     // Looking at a db backend, possibly sqlite to hold the "server configurations"
     const guildId = guild.id;
