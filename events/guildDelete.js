9ac61b97d2aa92fdb5900cadda3e6807f51f9271
Vertig0ne <chris@Chris-Retina.local>
2017-12-04 18:15:50 +0000

    Stop using process.cwd, does not help with Docker builds

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -5,7 +5,7 @@ module.exports = (guild) => {
     // We have joined a new discord server, we need to populate and prepare for configuration.
     // Looking at a db backend, possibly sqlite to hold the "server configurations"
     const guildId = guild.id;
-    let db = new sqlite3.Database(`${process.cwd()}${coreSettings["path"]}/settings.sqlite`);    
+    let db = new sqlite3.Database(`${coreSettings["path"]}/settings.sqlite`);    
     db.serialize(() => 
     {
         db.run(`DELETE FROM guildSettings WHERE guildId = ${guildId}`);
