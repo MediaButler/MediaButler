bac3c15ca7b0cd80220c1392ee9bd3284dcee45f
Vertig0ne <chris@Chris-Retina.local>
2017-12-05 03:02:59 +0000

    First real test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,19 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+
+describe('PlexPy Tests', function() {
+  describe('Can get NowPlaying from PlexPy', function() {
+    it('Gets data from API', function() {
+      const guildId = 0;
+      const getNowPlaying = require('../util/plexpy/getNowPlaying');
+      getNowPlaying(guildId, true)
+        .then((res) => {
+          assert.equal(true, (res != undefined));
+          assert.equal(1, res.data.stream_count);
+          assert.equal(0, res.data.stream_count_transcode);
+          assert.equal(1, res.data.stream_count_direct_play);
+          assert.equal(0, res.data.stream_count_direct_stream);
+          assert.equal(1, res.data.sessions.length);
+        });
     });
   });
 });
