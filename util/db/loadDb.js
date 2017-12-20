0563d1a295debf80d22f45d1aa7dd8fc28634445
Vertig0ne <chris@Chris-Retina.local>
2017-12-20 00:15:19 +0000

    save after loading defaults

--- util/db/loadDb.js
+++ util/db/loadDb.js
@@ -1,17 +1,20 @@
 const fs = require('fs');
 const coreSettings = require(`${process.cwd()}/settings.json`);
+const saveDb = require('./saveDb');
 module.exports = (client) =>  {
   client.guilds.forEach(guild => {
     fs.readFile(`${coreSettings['path']}/${guild.id}.json`, (err, data) => {  
       if (err) {
-        fs.readFile(`${coreSettings['path']}/default.json`, (err, data) => {
-          guild.settings = JSON.parse(data);
+        fs.readFile(`${coreSettings['path']}/default.json`, (err, data2) => {
+          guild.settings = JSON.parse(data2);
+          saveDb(client);
           console.log(`Loaded default settings for ${guild.id}`);
           return;      
         });
+      } else {
+        guild.settings = JSON.parse(data);
+        console.log(`Loaded settings for ${guild.id}`);
       }
-      guild.settings = JSON.parse(data);
-      console.log(`Loaded settings for ${guild.id}`);
     });
   });
 };
