71bc3a8aeab73290c5908b15317091a581346e8c
Vertig0ne <chris@Chris-Retina.local>
2017-12-20 02:03:16 +0000

    cleanup

--- commands/plex/config.js
+++ commands/plex/config.js
@@ -4,17 +4,13 @@ const getPin = require('../../util/plex/getPin');
 const getAuthToken = require('../../util/plex/getAuthToken');
 const saveDb = require('../../util/db/saveDb');
 exports.run = (bot, msg, args = []) => {
-  let plexurl;
-  if (msg.guild.settings.plex.url) plexurl = msg.guild.settings.plex.url ;
+  let plexurl = [];
+  if (msg.guild.settings.plex.url) plexurl[0] = msg.guild.settings.plex.url ;
   if (args != '') plexurl = args;
-  if (plexurl == '') { msg.channel.send('ERR: No Plex URL defined'); return; }
+  if (plexurl == []) { msg.channel.send('ERR: No Plex URL defined'); return; }
   setPlexUrl(msg.guild, plexurl[0]).then(() => {
-    plexurl = args;
     getApi(msg.guild, false)
       .then((d) => {
-        console.log(msg.guild.settings);
-        console.log(d.authToken);
-        console.log(msg.guild.settings.plex.pinToken);
         if (d.authToken == undefined && msg.guild.settings.plex.pinToken == undefined) { 
           getPin(msg.guild)
             .then((pinObj) => { 
@@ -27,15 +23,12 @@ exports.run = (bot, msg, args = []) => {
         if (d.authToken == undefined && msg.guild.settings.plex.pinToken != undefined) {
           getAuthToken(msg.guild)
             .then((token) => {
-              console.log(token);
               msg.guild.settings.plex.token = token;
               msg.channel.send('Sucessfully processed plex token. Please run command again and we will work.');
               saveDb(bot);
             });
           return;
         }
-        
-        console.log(d);
         msg.channel.send('Plex sucessfully configured');
       });
   });
