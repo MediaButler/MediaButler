959017d977dd77b82d13eb357bad98247a789381
Vertig0ne <chris@Chris-Retina.local>
2017-12-25 20:36:00 +0000

    uuidv4 filled out on guild creation

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,6 +1,8 @@
 const saveDb = require('../util/db/saveDb');
+const uuid = require('uuid-v4');
 module.exports = (guild) => {
   guild.settings = require('../config/default.json');
+  guild.settings.uuidv4 = uuid();  
   saveDb(guild.client);
   guild.owner.send(`Hello, I am ${guild.client.user.username} your personal Media Butler!\nTo get going we are going to need to set a few settings, please look at the set command and our github wiki to get going`);        
 };
