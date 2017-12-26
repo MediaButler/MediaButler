ec21d925447cf11f5d8b16f9ac450cf82f24df29
Vertig0ne <chris@Chris-Retina.local>
2017-12-26 11:11:34 +0000

    fixes #124

--- commands/plex/help.js
+++ commands/plex/help.js
@@ -1,14 +1,13 @@
-const settings = require(`${process.cwd()}/settings.json`);
 exports.run = (bot, msg, args = []) => {
   if (!args[0]) {
     const commandNames = Array.from(bot.plexCommands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    msg.channel.send(`= Command List =\n\n[Use ${settings.prefix}plex help <command> for details]\n\n${bot.plexCommands.map(c => `${settings.prefix}plex ${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, { code: 'asciidoc' });
+    msg.channel.send(`= Command List =\n\n[Use ${msg.guild.settings.prefix}plex help <command> for details]\n\n${bot.plexCommands.map(c => `${msg.guild.settings.prefix}plex ${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, { code: 'asciidoc' });
   } else {
     let command = args[0];
     if (bot.plexCommands.has(command)) {
       command = bot.plexCommands.get(command);
-      msg.channel.send(`= ${command.help.name} = \n\n[${command.help.description}]\n\nusage :: ${settings.prefix}${command.help.usage}`, { code: 'asciidoc' });
+      msg.channel.send(`= ${command.help.name} = \n\n[${command.help.description}]\n\nusage :: ${msg.guild.settings.prefix}${command.help.usage}`, { code: 'asciidoc' });
     }
   }
 };
--- commands/tv/help.js
+++ commands/tv/help.js
@@ -1,15 +1,13 @@
-const settings = require(`${process.cwd()}/settings.json`);
-
 exports.run = (bot, msg, args = []) => {
   if (!args[0]) {
     const commandNames = Array.from(bot.tvCommands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    msg.channel.send(`= Command List =\n\n[Use ${settings.prefix}tv help <command> for details]\n\n${bot.tvCommands.map(c => `${settings.prefix}tv ${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, { code: 'asciidoc' });
+    msg.channel.send(`= Command List =\n\n[Use ${msg.guild.settings.prefix}tv help <command> for details]\n\n${bot.tvCommands.map(c => `${msg.guild.settings.prefix}tv ${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, { code: 'asciidoc' });
   } else {
     let command = args[0];
     if (bot.tvCommands.has(command)) {
       command = bot.tvCommands.get(command);
-      msg.channel.send(`= ${command.help.name} = \n\n[${command.help.description}]\n\nusage :: ${settings.prefix}${command.help.usage}`, { code: 'asciidoc' });
+      msg.channel.send(`= ${command.help.name} = \n\n[${command.help.description}]\n\nusage :: ${msg.guild.settings.prefix}${command.help.usage}`, { code: 'asciidoc' });
     }
   }
 };
