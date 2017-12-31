d5d2dded4932589163bb67672ac83abef2cd446f
Vertig0ne <github@chrish.net>
2017-12-31 19:44:54 +0000

    Actually build uuid on guild invite and save file

--- util/discordCreateGuild.js
+++ util/discordCreateGuild.js
@@ -1,10 +1,13 @@
 const uuid = require('uuid-v4');
+const saveDb = require('./db/saveDb');
 module.exports = (guild) => 
 {
   const p = new Promise((resolve, reject) => 
   {
     const myUuid = uuid();
     guild.settings = require('../config/default.json');
+    guild.uuidv4 = myUuid;
+    saveDb(guild.client);
     resolve();
   });
   return p;
