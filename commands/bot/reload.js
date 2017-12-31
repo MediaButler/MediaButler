76fe25b232baa782a7e00de7f95902038a1206e4
Vertig0ne <github@chrish.net>
2017-12-31 19:43:55 +0000

    Refactor reload command, hopefully looks looks a bit easier to handle

--- commands/bot/reload.js
+++ commands/bot/reload.js
@@ -1,52 +1,37 @@
 const fs = require('fs');
-exports.run = (client, msg, args) => {
-  let command;
-  if (client.commands.has(args[0])) {
-    command = args[0];
-  } else if (client.aliases.has(args[0])) {
-    command = client.aliases.get(args[0]);
-  }
-  if (!command) {
-    fs.readdir('./commands/', (err, files) => {
-      if (err) msg.channel.send(err);
-      msg.channel.send(`Reloading a total of ${files.length} commands.`);
-      files.forEach(f => {
-        const props = require(`./${f}`);
-        msg.channel.send(`Loading Command: ${props.help.name}.`)
-          .then(m => {
-            client.reload(props.help.name)
-              .then(() => {
-                m.edit(`Successfully reloaded: ${props.help.name}`);
-              })
-              .catch(e => {
-                m.edit(`Command reload failed: ${props.help.name}\n\`\`\`${e.stack}\`\`\``);
-              });
-          });
+exports.run = (client, msg, args = []) => {
+  msg.channel.startTyping();
+  let sendMessage = '```asciidoc\n';
+  fs.readdir(`${process.cwd()}/commands/`, (err, files) => {
+    if (err) msg.channel.send(err);
+    sendMessage += `Reloading a total of ${files.length} commands\n\n`;
+    msg.channel.send(`${sendMessage}\`\`\``)
+      .then((m) => {
+        files.forEach(f => {
+          const props = require(`${process.cwd()}/commands/${f}`);
+          m.edit(`${sendMessage}\nLoading Command: ${props.help.name}.\n\`\`\``);
+          client.reload(props.help.name)
+            .then(() => {
+              sendMessage += `Sucessfully reloaded: ${props.help.name}\n`;
+              m.edit(`${sendMessage}\`\`\``);
+            })
+            .catch(e => {
+              sendMessage += `\n\nCommand reload failed: ${props.help.name}\n\`\`\`${e.stack}\`\`\``;
+              m.edit(`${sendMessage}\`\`\``);
+            });
+        });
       });
-    }); //return msg.channel.send(`I cannot find the command: ${args[0]}`);
-  } else {
-    msg.channel.send(`Reloading: ${command}`)
-      .then(m => {
-        client.reload(command)
-          .then(() => {
-            m.edit(`Successfully reloaded: ${command}`);
-          })
-          .catch(e => {
-            m.edit(`Command reload failed: ${command}\n\`\`\`${e.stack}\`\`\``);
-          });
-      });
-  }
+  });
+  msg.channel.stopTyping();
 };
-
 exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: ['r'],
   permLevel: 4
 };
-
 exports.help = {
   name: 'reload',
-  description: 'Reloads the command file, if it\'s been updated or modified.',
-  usage: 'bot reload <commandName>'
+  description: 'Reloads bot code',
+  usage: 'bot reload'
 };
