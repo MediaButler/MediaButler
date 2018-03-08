64452210768bbe1924cd0a00564bb88678a0bb8b
Rob Gökemeijer <grob1998@me.com>
2018-03-09 00:28:47 +0100

    whoops

--- util/emby/createNowPlayingMovieModal.js
+++ util/emby/createNowPlayingMovieModal.js
@@ -4,7 +4,7 @@ module.exports = (guild, movieItem) => {
   const i = new Discord.RichEmbed()
     .setTitle(movieItem.Name)
     .setDescription(movieItem.Overview)
-    .setThumbnail(guild.emby.external_url + "/Items/" + movieItem.Id + "/Images/Primary")
+    .setThumbnail(guild.settings.emby.external_url + "/Items/" + movieItem.Id + "/Images/Primary")
     .setColor(3447003)
     .setTimestamp()
   return i;
