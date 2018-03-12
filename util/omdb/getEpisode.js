be5117928cfe77a1da060902d9aafbc5ef9a8951
Rob Gökemeijer <grob1998@me.com>
2018-03-12 23:49:59 +0100

    Fix urlbase regex groups for Sonarr and Radarr
    Also the tonight command returned no airing episodes, I think that this is because of the difference in time zones perhaps, I increased it to +2 and now it does show correctly, but I think it might show to many episodes for other users?
    Removed some logging

--- util/omdb/getEpisode.js
+++ util/omdb/getEpisode.js
@@ -3,9 +3,6 @@ module.exports = (show, season, episode) =>
 {
   const p = new Promise((resolve, reject) =>
   {
-  	console.log(show.join(' '))
-  	console.log(season)
-  	console.log(episode)
     const url = `http://www.omdbapi.com/?t=${show.join(' ')}&Season=${season}&Episode=${episode}&apikey=5af02350&type=series`;
     request(url, function(e, r, b) {
       if (!e && r.statusCode === 200) resolve(JSON.parse(b));
--- util/sonarr/getTonight.js
+++ util/sonarr/getTonight.js
@@ -7,7 +7,7 @@ module.exports = (guild) => {
       {
         const today = new Date();
         const yesterday = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1);
-        const tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
+        const tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 2);
         today.setUTCHours(20);
         tomorrow.setUTCHours(06);
         sonarr.get('calendar', { 'start': today.toISOString(), 'end': tomorrow.toISOString()})
