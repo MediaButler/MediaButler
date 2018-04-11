cb506b5a3c72ac7bdf28f657987f5c9a06cd455f
vertig0ne <23177238+vertig0ne@users.noreply.github.com>
2018-04-11 03:39:39 +0100

    stop shuffling the current playing song

--- util/music/shuffleQueue.js
+++ util/music/shuffleQueue.js
@@ -1,4 +1,5 @@
 module.exports = (message) => {
+  const item = message.guild.mediaQueue.shift();
   let counter = message.guild.mediaQueue.length;
   while (counter > 0) {
     const index = Math.floor(Math.random() * counter);
@@ -7,5 +8,6 @@ module.exports = (message) => {
     message.guild.mediaQueue[counter] = message.guild.mediaQueue[index];
     message.guild.mediaQueue[index] = temp;
   }
+  message.guild.mediaQueue.unshift(item);
   return message.guild.mediaQueue;
 };
