ae3c6b948878d5174752a89b5b36b978cc27e8e6
vertig0ne <23177238+vertig0ne@users.noreply.github.com>
2018-05-09 02:43:00 +0100

    slight alteration to ping command

--- app/commands/guild/bot/ping.js
+++ app/commands/guild/bot/ping.js
@@ -1,16 +1,14 @@
-const l = require('../../../config.json').defaultLanguage;
-const lang = require('../../../lang')(l);
-
 module.exports = {
     run: (client, message, args, perms) => {
-        message.channel.send(lang.get('bot.ping.ping'))
+        const languageService = new languageService(message.guild.settings.lang);
+        message.channel.send(languageService.get('bot.ping.ping'))
             .then(msg => {
-                msg.edit(lang.get('bot.ping.reply', msg.createdTimestamp - message.createdTimestamp));
+                msg.edit(languageService.get('bot.ping.reply', msg.createdTimestamp - message.createdTimestamp));
             });
     },
     conf: {
-        name: lang.get('bot.ping.name'),
-        alias: [lang.get('bot.ping.alias')],
-        description: lang.get('bot.ping.description')
+        name: 'bot.ping.name',
+        alias: 'bot.ping.alias',
+        description: 'bot.ping.description'
     }
 }
