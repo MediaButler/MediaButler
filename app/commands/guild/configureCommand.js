6170539060ab4659dd0bce52ceca2e3e985bca65
vertig0ne <23177238+vertig0ne@users.noreply.github.com>
2018-05-13 01:44:12 +0100

    configureCommand laid out a little better. Added rudimentary updt config

--- app/commands/guild/configureCommand.js
+++ app/commands/guild/configureCommand.js
@@ -29,13 +29,18 @@ module.exports = class configureCommand extends command {
         this.embed.setAuthor(message.guild.name);
         args = this.parseArgs(args);
 
-        if (args.element != 'checkauth' && (!args.url || !args.apikey || !args.element)) throw new Error('Could not parse url, api key or item to configure');
+        if (args.element != 'checkauth' && args.element != 'update' && (!args.url || !args.apikey || !args.element)) throw new Error('Could not parse url, api key or item to configure');
 
         const msg = await message.reply(this.embed);
         let r;
         switch (args.element) {
             case 'checkauth':
                 r = await this.checkPlexAuth(msg, args);
+                break;
+            case 'update':
+                const s = this.client.settingsService.get(msg.guild.id);
+                if (!s.organizr.url && !s.organizr.apikey) throw new Error('No Organizr configuration saved');
+                r = await this.configureOrganizr(msg, s.organizr);
                 break;
             case 'organizr':
                 r = await this.configureOrganizr(msg, args);
@@ -46,13 +51,11 @@ module.exports = class configureCommand extends command {
                 r = await this.configureTautulli(msg, args);
                 break;
             case 'sonarr':
-                if (!args.rootpath || !args.profile) throw new Error('Root Path or Profile name not specifified');
                 this.embed.setDescription(this.languageService.get(this.lang, 'bot.configure.configuring', this.languageService.get(this.lang, 'bot.configure.sonarr')));
                 msg.edit(this.embed);
                 r = await this.configureSonarr(msg, args);
                 break;
             case 'radarr':
-                if (!args.rootpath || !args.profile) throw new Error('Root Path or Profile name not specifified');
                 this.embed.setDescription(this.languageService.get(this.lang, 'bot.configure.configuring', this.languageService.get(this.lang, 'bot.configure.radarr')));
                 msg.edit(this.embed);
                 r = await this.configureSonarr(msg, args);
@@ -73,7 +76,7 @@ module.exports = class configureCommand extends command {
         this.embed.setAuthor(msg.guild.name);
         msg.edit(this.embed);
 
-        const o = new organizrService(args)
+        const o = new organizrService(args);
         const c = await o.getConfig();
         if (c) {
             const s = this.client.settingsService.get(msg.guild.id);
@@ -85,10 +88,14 @@ module.exports = class configureCommand extends command {
             if (c.tautulli.url && c.tautulli.apikey) this.configureTautulli(msg, c.tautulli);
             else { this.embed.addField(this.languageService.get(this.lang, 'bot.configure.tautulli'), this.languageService.get(this.lang, 'bot.configure.insufficent'), true); msg.edit(this.embed); }
 
-            if (c.sonarr.url && c.sonarr.apikey && c.sonarr.defaultRootPath && c.sonarr.defaultProfile) this.configureSonarr(msg, c.sonarr);
+            if (c.sonarr.defaultProfile) c.sonarr.profile = c.sonarr.defaultProfile;
+            if (c.sonarr.defaultRootPath) c.sonarr.rootpath = c.sonarr.defaultRootPath;
+            if (c.sonarr.url && c.sonarr.apikey && c.sonarr.rootpath && c.sonarr.profile) this.configureSonarr(msg, c.sonarr);
             else { this.embed.addField(this.languageService.get(this.lang, 'bot.configure.sonarr'), this.languageService.get(this.lang, 'bot.configure.insufficent'), true); msg.edit(this.embed); }
 
-            if (c.radarr.url && c.radarr.apikey && c.radarr.defaultRootPath && c.radarr.defaultProfile) this.configureRadarr(msg, c.radarr);
+            if (c.radarr.defaultProfile) c.radarr.profile = c.radarr.defaultProfile;
+            if (c.radarr.defaultRootPath) c.radarr.rootpath = c.radarr.defaultRootPath;
+            if (c.radarr.url && c.radarr.apikey && c.radarr.rootpath && c.radarr.profile) this.configureRadarr(msg, c.radarr);
             else { this.embed.addField(this.languageService.get(this.lang, 'bot.configure.radarr'), this.languageService.get(this.lang, 'bot.configure.insufficent'), true); msg.edit(this.embed); }
 
             if (c.plex.url) this.configurePlex(msg, c.plex);
@@ -101,60 +108,108 @@ module.exports = class configureCommand extends command {
         return msg;
     }
 
+    _updateSettings(element, id, args) {
+        const s = this.client.settingsService.get(id);
+        let update = eval(`s.${element}`);
+        update = args;
+        this.client.settingsService.set(id, s);
+    }
+
+    async _testTautulli(msg, args) {
+        try {
+            const t = new tautulliService(args);
+            const e = await t.getNowPlaying();
+            if (e.message && e.message.startsWith('Invalid')) {
+                return false;
+            } else return true;
+        } catch (err) { throw err; }
+    }
+
+    async _testSonarr(msg, args) {
+        try {
+            const s = new sonarrService(args);
+            const c = await s.getMonthCalendar();
+            if (c[0].seriesId) {
+                return true;
+            } else return false;
+        } catch (err) { throw err; }
+    }
+
+    async _testRadarr(msg, args) {
+        try {
+            const s = new radarrService(args);
+            const c = await s.getMonthCalendar();
+            if (c[0].tmdbId) {
+                return true;
+            } else return false;
+        } catch (err) { throw err; }
+    }
+
     async configureTautulli(msg, args) {
-        const t = new tautulliService(args);
-        const e = await t.getNowPlaying();
-        if (e.result == 'success') {
-            this.embed.addField(this.languageService.get(this.lang, 'bot.configure.tautulli'), this.languageService.get(this.lang, 'bot.configure.success'), true);
-            const s = this.client.settingsService.get(msg.guild.id);
-            s.tautulli = { 'url': args.url, 'apikey': args.apikey };
-            this.client.settingsService.set(msg.guild.id, s);
+        try {
+            if (await this._testTautulli(msg, args)) {
+                this._updateSettings('tautulli', msg.guild.id, { 'url': args.url, 'apikey': args.apikey });
+                this.embed.addField(this.languageService.get(this.lang, 'bot.configure.tautulli'), this.languageService.get(this.lang, 'bot.configure.success'), true);
+                return msg.edit(this.embed);
+            }
+            throw new Error('Unable to connect to Tautulli');
+        }
+        catch (err) {
+            this.embed.addField(this.languageService.get(this.lang, 'bot.configure.tautulli'), this.languageService.get(this.lang, 'bot.configure.failed', `${err.name}: ${err.message}`), true);
             return msg.edit(this.embed);
         }
-        this.embed.addField(this.languageService.get(this.lang, 'bot.configure.tautulli'), this.languageService.get(this.lang, 'bot.configure.failed'), true);
-        return msg.edit(this.embed);
     }
 
     async configureSonarr(msg, args) {
-        const s = new sonarrService(args);
-        const c = await s.getMonthCalendar();
-        if (c[0].seriesId) {
-            this.embed.addField(this.languageService.get(this.lang, 'bot.configure.sonarr'), this.languageService.get(this.lang, 'bot.configure.success'), true);
-            const se = this.client.settingsService.get(msg.guild.id);
-            se.sonarr = { 'url': args.url, 'apikey': args.apikey };
-            this.client.settingsService.set(msg.guild.id, se);
+        try {
+            console.log(args);
+            if (!args.rootpath || !args.profile) throw new Error('Root Path or Profile name not specifified');
+            if (await this._testSonarr(msg, args)) {
+                this._updateSettings('sonarr', msg.guild.id, { 'url': args.url, 'apikey': args.apikey, 'profile': args.profile, 'rootPath': args.rootpath });
+                this.embed.addField(this.languageService.get(this.lang, 'bot.configure.sonarr'), this.languageService.get(this.lang, 'bot.configure.success'), true);
+                return msg.edit(this.embed);
+            }
+            throw new Error('Unable to connect to Sonarr');
+        }
+        catch (err) {
+            this.embed.addField(this.languageService.get(this.lang, 'bot.configure.sonarr'), this.languageService.get(this.lang, 'bot.configure.failed', `${err.message}`), true);
             return msg.edit(this.embed);
         }
-        this.embed.addField(this.languageService.get(this.lang, 'bot.configure.sonarr'), this.languageService.get(this.lang, 'bot.configure.failed'), true);
-        return msg.edit(this.embed);
     }
 
     async configureRadarr(msg, args) {
-        const r = new radarrService(args);
-        const c = await r.getMonthCalendar();
-        if (c[0].tmdbId) {
-            this.embed.addField(this.languageService.get(this.lang, 'bot.configure.radarr'), this.languageService.get(this.lang, 'bot.configure.success'), true);
-            const se = this.client.settingsService.get(msg.guild.id);
-            se.radarr = { 'url': args.url, 'apikey': args.apikey, };
-            this.client.settingsService.set(msg.guild.id, se);
+        try {
+            if (!args.rootpath || !args.profile) throw new Error('Root Path or Profile name not specifified');
+            if (await this._testRadarr(msg, args)) {
+                this._updateSettings('radarr', msg.guild.id, { 'url': args.url, 'apikey': args.apikey, 'profile': args.profile, 'rootPath': args.rootpath });
+                this.embed.addField(this.languageService.get(this.lang, 'bot.configure.radarr'), this.languageService.get(this.lang, 'bot.configure.success'), true);
+                return msg.edit(this.embed);
+            }
+            throw new Error('Unable to connect to Radarr');
+        }
+        catch (err) {
+            this.embed.addField(this.languageService.get(this.lang, 'bot.configure.radarr'), this.languageService.get(this.lang, 'bot.configure.failed', `${err.message}`), true);
             return msg.edit(this.embed);
         }
-        this.embed.addField(this.languageService.get(this.lang, 'bot.configure.radarr'), this.languageService.get(this.lang, 'bot.configure.failed'), true);
-        return msg.edit(this.embed);
     }
 
     async configurePlex(msg, args) {
-        if (!args.uuid) args.uuid = uuid();
-        const p = new plexTokenService(args, this.client);
-        const token = await p.getPin();
-        if (token) {
-            const se = this.client.settingsService.get(msg.guild.id);
-            se.plex = { 'uuid': args.uuid, 'url': args.url, 'pinToken': token };
-            this.client.settingsService.set(msg.guild.id, se);
-            this.embed.addField(this.languageService.get(this.lang, 'bot.configure.plex'), this.languageService.get(this.lang, 'bot.configure.webToken', { 'url': 'https://plex.tv/pin', 'code': token.code }), true);
+        try {
+            if (!args.uuid) args.uuid = uuid();
+            const p = new plexTokenService(args, this.client);
+            const token = await p.getPin();
+            if (token) {
+                const se = this.client.settingsService.get(msg.guild.id);
+                se.plex = { 'uuid': args.uuid, 'url': args.url, 'pinToken': token };
+                this.client.settingsService.set(msg.guild.id, se);
+                this.embed.addField(this.languageService.get(this.lang, 'bot.configure.plex'), this.languageService.get(this.lang, 'bot.configure.webToken', { 'url': 'https://plex.tv/pin', 'code': token.code }), true);
+                return msg.edit(this.embed);
+            }
+        }
+        catch (err) {
+            this.embed.addField(this.languageService.get(this.lang, 'bot.configure.plex'), this.languageService.get(this.lang, 'bot.configure.failed', `${err.message}`), true);
             return msg.edit(this.embed);
         }
-
     }
 
     async checkPlexAuth(msg, args) {
@@ -184,10 +239,9 @@ module.exports = class configureCommand extends command {
         const r = { url: false, apikey: false, element: false, rootpath: false, profile: false };
         args = args.split(',');
         args.forEach((arg) => {
-            if (arg == '-checkauth') {
-                r.element = 'checkauth';
-                return r;
-            }
+            if (arg == '-checkauth') { r.element = 'checkauth'; return r; }
+            if (arg == '-update') { r.element = 'update'; return r; }
+
             const urlRegex = /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[A-Z0-9+&@#/%=~_|$])/gi;
             const u = urlRegex.exec(arg);
             if (u && !r.url) r.url = u.input;
@@ -210,5 +264,4 @@ module.exports = class configureCommand extends command {
         });
         return r;
     }
-
 }
